686aa0d34f4e8d3d782fe7bf9f48b032da96c4cd
Shaxnozaxon <s.norxojayeva@newuu.uz>
2025-09-03 00:00:48 +0500

    Add files via upload

---
+++ section7.cpp
@@ -0,0 +1,13 @@
+/*
+question2:
+When using enums instead of string or integer constants, only clearly defined values are used,
+protects against sending incorrect strings.
+
+question3:
+It is impossible to give an incorrect value.
+Anyone who sees the code will immediately understand its meaning.
+
+ question4:
+C++ scoped enum: strong type safety, but no methods or fields.
+Java enum: similar to an object-methods, fields, and constructors can be added.
+ */
