95ff0ef2893bcbaf108756d42021bb8316860fe9
Shaxnozaxon <s.norxojayeva@newuu.uz>
2025-09-12 16:58:07 +0500

    Create Section5.dart
    All of the tasks given in sections are done.

---
+++ Assignment2/Section5.dart
@@ -0,0 +1,48 @@
+//TASK1
+/*
+Author: Gemini
+Date: September 8, 2025
+This program demonstrates how to use comments in Dart.
+*/
+void main () {
+// This is a single-line comment. It explains the next line of code. String message = ’Comments make code readable.’;
+print(message); }
+
+//TASK2
+void main () {
+// variable planet with type of String, and this variable stores value "Earth" 
+String planet = ’Earth ’;
+print(’We live on planet $planet.’);
+}
+
+//TASK3
+/* 
+Shaxnozaxon Norxo'jayeva
+Current date: Fri 12 Sep 16:49
+This is my first Dart function.
+*/
+void sayHello () {
+print(’Hello from a function!’);
+}
+void main () { sayHello();
+}
+
+//TASK4
+void main () {
+//print(’This message should not appear.’);
+print('This message should appear.');
+}
+
+//TASK5
+import ’dart:math ’;
+/// The function calculates the area of a circle with the given radius.
+return pi * radius * radius; }
+void main () { print(calculateCircleArea(5.0));
+}
+
+//TASK6
+void main () {
+// This variable stores a very important universal constant. 
+int speedOfLight = 299792458; //in meters per second
+print(speedOfLight);
+}
