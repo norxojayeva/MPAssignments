1fb746d68a6dca1c7227eaf1a16d2672298998ba
Shaxnozaxon <s.norxojayeva@newuu.uz>
2025-09-12 16:11:43 +0500

    Create Section3.dart
    Section3 tasks are done.

---
+++ Assignment2/Section3.dart
@@ -0,0 +1,66 @@
+//TASK1
+void main () {
+    int number = 15;
+    if(number>10){
+      print('The number is greater than 10.'); 
+    } else {
+      print('The number is not greater than 10.'); 
+    }
+}
+
+//TASK2
+void main () {
+    int studentScore = 75;
+    if(studentScore>60){
+      print('Pass'); 
+    } else {
+      print('Fail'); 
+    }
+}
+
+//TASK3
+void main () {
+    int hour= 14;
+    if(hour<12){
+      print('Good morning'); 
+    } else if(hour>=12 && hour<=18) {
+      print('Good afternoon'); 
+    } else {
+        print('Good evening');
+    }
+}
+
+//TASK4
+void main () {
+    int i;
+    for(i=1; i<=10; i++){
+        print(i);
+    }
+}
+
+//TASK5
+void main () {
+    int i;
+    for(i=5; i>=1; i--){
+        print(i);
+    }
+    print('Liftoff!');
+}
+
+//TASK6
+void main () {
+    int i;
+    for(i=1; i<=20; i++){
+        if(i%3==0){
+        print(i);
+    }
+}
+}
+
+
+
+
+
+
+
+
