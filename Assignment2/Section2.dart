48d3bcc61c248f9b8a9ddf0ef5379c62dea179d8
Shaxnozaxon <s.norxojayeva@newuu.uz>
2025-09-12 15:49:17 +0500

    Create Section2.dart
    Section2 tasks are done.

---
+++ Assignment2/Section2.dart
@@ -0,0 +1,39 @@
+//TASK1
+void main(){
+    int age=20;
+    print('I\'m $age years old.');
+}
+
+//TASK2
+void main(){
+    String favoriteCity="Almalyk";
+    print('My favorite city is $favoriteCity !');
+}
+
+//TASK3
+void main(){
+    double bookPrice=15.99;
+    print('The price of the book is $bookPrice USD.');
+}
+
+//TASK4
+void main(){
+    bool isLearningDart=true;
+    print('Am I learning Dart? The answer is $isLearningDart.');
+}
+
+//TASK5
+void main(){
+    String weather="Sunny";
+    int temperature=32;
+    print('Today\’s weather is $weather and the temperature is $temperature degrees Celsius.');
+}
+
+//TASK6
+void main(){
+    var numberOfApples=10;
+    print(numberOfApples);
+    
+    numberOfApples=12;
+    print(numberOfApples);
+}
