de7c87ec88230291069f73f0a26e0a61daf67259
Shaxnozaxon <s.norxojayeva@newuu.uz>
2025-09-12 15:27:23 +0500

    Update and rename Section1 to Section1.dart
    Section 1 tasks are done.

---
+++ Assignment2/Section1.dart
@@ -0,0 +1,30 @@
+// TASK1 
+void main(){
+  print('Hello, World!');
+}
+
+// TASK2 
+void main(){
+  print('Hi, it's me.\n Shaxnozaxon Norxo\'jayeva');
+}
+
+//Task3 
+void main(){
+  print('Welcome to Dart Programming.');
+  print('Let\'s start coding!');
+}
+
+// TASK 4  
+void main() {
+  print('1 Amir Temur Avenue\nTashkent, Uzbekistan\n100000');
+}
+
+//TASK5
+void main(){
+  print('The current year is 2025.');
+}
+
+//TASK6
+void main(){
+  print('Welcome, students of New Uzbekistan University in Tashkent!');
+}
