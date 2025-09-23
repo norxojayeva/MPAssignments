22ab7ff3119f091334fb4dd587f19d4583f688a1
Shaxnozaxon <s.norxojayeva@newuu.uz>
2025-09-23 23:01:49 +0500

    Create Task5_Challenge1.dart
    Challenge1 is done

---
+++ Assignment4/Task5_Challenge1.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Lab 5 - Task 5 Challenge 1")),
+        body: const Center(
+          child: UserProfileCard(),
+        ),
+      ),
+    );
+  }
+}
+
+class UserProfileCard extends StatelessWidget {
+  const UserProfileCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.grey[250],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              const CircleAvatar(
+                radius: 35,
+                child:  const Text("A", style:TextStyle(fontSize:22)), 
+              ),
+              const SizedBox(width: 25),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text("User Name", style: TextStyle(fontSize: 21, fontWeight:                          FontWeight.bold)),
+                    Text("@username", style: TextStyle(color: Colors.grey)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 40),
+          const Text(
+            "A short bio about the user goes here...",
+            style: TextStyle(fontSize: 20),
+          ),
+          const SizedBox(height: 40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(onPressed: () {}, child: const Text("Follow")),
+              ElevatedButton(onPressed: () {}, child: const Text("Message")),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
