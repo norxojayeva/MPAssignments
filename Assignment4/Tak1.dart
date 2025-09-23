c4cf700f1cc4f9881327288b16843cefa5590338
Shaxnozaxon <s.norxojayeva@newuu.uz>
2025-09-23 10:36:24 +0500

    Create Tak1.dart
    All parts from Task1 is done.

---
+++ Assignment4/Tak1.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text("My Assignment4.")),
+        body: const Center(child: MyWidget()),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(16.0),
+      height: 150,
+      width: 100,
+      child: const Icon(Icons.home, color: Colors.white),
+      decoration: BoxDecoration(
+        color: Colors.redAccent,
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+  }
+}
