64ebc8f879b75aa36ed5a9e61e04d093ae230284
Shaxnozaxon <s.norxojayeva@newuu.uz>
2025-09-30 10:54:35 +0500

    Create Task4.dart

---
+++ Assignment5/Section1/Task4.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: LikeButton(),
+        ),
+      ),
+    );
+  }
+}
+
+class LikeButton extends StatefulWidget {
+  const LikeButton({super.key});
+
+  @override
+  State<LikeButton> createState() => _LikeButtonState();
+}
+
+class _LikeButtonState extends State<LikeButton> {
+  bool _isLiked = false; 
+
+  void _toggleLike() {
+    setState(() {
+      _isLiked = !_isLiked; 
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        _isLiked ? Icons.favorite : Icons.favorite_border,
+        color: _isLiked ? Colors.red : Colors.grey,
+        size: 80,
+      ),
+      onPressed: _toggleLike, 
+    );
+  }
+}
